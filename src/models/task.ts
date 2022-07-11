b86a9ac70ed37fce5b324edc0bf21e916d5e88a3
fanuelramos <fanuelramos111@gmail.com>
2022-07-11 14:18:51 +0100

    Create task model

---
+++ src/models/task.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface ITask {
+    name: String;
+    description: Text;
+    start: Date;
+    finish: Date;
+    status: String;
+}
+
+export interface ITaskModel extends ITask, Document {}
+
+const TaskSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        start: {
+            type: Date,
+            required: true
+        },
+        finish: {
+            type: Date,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        }
+    }, 
+    {
+        versionKey: false
+    }
+);
+
+export default mongoose.model<ITaskModel>('Task', TaskSchema);
